956cc1978b1ca77a41499243e2ebefdf8b8a3371
James Juett <jjuett@umich.edu>
2021-01-08 23:01:07 -0500

    blah 2

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -8,7 +8,7 @@ export const auth_config = {
   google: {
     clientID: assertExists(process.env.GOOGLE_CLIENT_ID),
     clientSecret: assertExists(process.env.GOOGLE_CLIENT_SECRET),
-    callbackURL: "https://localhost/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL!
   },
   
   jwt: {
